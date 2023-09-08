0683e09ca1fc782efd79b9202f9c25669d77de52
atonya-bravin <bravinatonya.brani@gmail.com>
2023-09-09 00:44:38 +0300

    Inclusion of dynamic slack_name and track data

--- User-Information-api/app.py
+++ User-Information-api/app.py
@@ -6,11 +6,11 @@ app = FastAPI()
 @app.get("/api")
 def respond(slack_name: str = Query(...), track: str = Query(...)):
     return {
-        "slack_name": "example_name",
+        "slack_name": f"{slack_name}",
         "current_day": "Monday",
         "utc_time": "2023-08-21T15:04:05Z",
-        "track": "backend",
-        "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext",
-        "github_repo_url": "https://github.com/username/repo",
+        "track": f"{track}",
+        "github_file_url": "https://github.com/atonya-bravin/HNGx-backend-track/blob/main/User-Information-api/app.py",
+        "github_repo_url": "https://github.com/atonya-bravin/HNGx-backend-track/tree/main",
         "status_code": 200
     }
