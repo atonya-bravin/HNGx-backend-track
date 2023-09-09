345267c3ed81d9d9972ab43e28fcaa06f153ee3e
atonya-bravin <bravinatonya.brani@gmail.com>
2023-09-09 23:32:41 +0300

    returning time as time and not string

--- User-Information-api/app.py
+++ User-Information-api/app.py
@@ -17,7 +17,7 @@ def respond(slack_name: str = Query(...), track: str = Query(...)):
     return {
         "slack_name": f"{slack_name}",
         "current_day": f"{day_of_week}",
-        "utc_time": f"{formatted_utc_time}",
+        "utc_time": formatted_utc_time,
         "track": f"{track}",
         "github_file_url": "https://github.com/atonya-bravin/HNGx-backend-track/blob/main/User-Information-api/app.py",
         "github_repo_url": "https://github.com/atonya-bravin/HNGx-backend-track/tree/main",
