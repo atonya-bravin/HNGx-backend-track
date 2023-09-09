562fd9d46363be7889e8ad639446b1b77297ba4c
atonya-bravin <bravinatonya.brani@gmail.com>
2023-09-09 11:31:26 +0300

    use of isoformat to create the utc-time

--- User-Information-api/app.py
+++ User-Information-api/app.py
@@ -1,23 +1,23 @@
 from fastapi import FastAPI, Query
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 
 app = FastAPI()
 
-
+now = datetime.now()
 day_of_week = datetime.now().strftime("%A")
 
-current_utc_time = datetime.utcnow()
+utc_time = now.replace(tzinfo=timezone.utc)
 
-formatted_utc_time = current_utc_time.strftime('%Y-%m-%d %H:%M:%S')
+iso_utc_time = utc_time.isoformat()[:-5] + 'Z'
 
 @app.get("/api")
 def respond(slack_name: str = Query(...), track: str = Query(...)):
     return {
         "slack_name": f"{slack_name}",
         "current_day": f"{day_of_week}",
-        "utc_time": f"{formatted_utc_time}",
+        "utc_time": f"{iso_utc_time}",
         "track": f"{track}",
         "github_file_url": "https://github.com/atonya-bravin/HNGx-backend-track/blob/main/User-Information-api/app.py",
         "github_repo_url": "https://github.com/atonya-bravin/HNGx-backend-track/tree/main",
