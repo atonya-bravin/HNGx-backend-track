d84bf7b5c76d406a0d57231f94b681de3849d85c
atonya-bravin <bravinatonya.brani@gmail.com>
2023-09-09 23:50:38 +0300

    returning time as a time object

--- User-Information-api/app.py
+++ User-Information-api/app.py
@@ -9,15 +9,14 @@ now = datetime.now()
 day_of_week = datetime.now().strftime("%A")
 
 current_utc_time = datetime.utcnow()
-
-formatted_utc_time = current_utc_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+utc_time_as_time = current_utc_time.time()
 
 @app.get("/api")
 def respond(slack_name: str = Query(...), track: str = Query(...)):
     return {
         "slack_name": f"{slack_name}",
         "current_day": f"{day_of_week}",
-        "utc_time": formatted_utc_time,
+        "utc_time": utc_time_as_time,
         "track": f"{track}",
         "github_file_url": "https://github.com/atonya-bravin/HNGx-backend-track/blob/main/User-Information-api/app.py",
         "github_repo_url": "https://github.com/atonya-bravin/HNGx-backend-track/tree/main",
