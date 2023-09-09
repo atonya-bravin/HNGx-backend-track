4075e3ec0e04307b13784971483e9d46f0adda29
atonya-bravin <bravinatonya.brani@gmail.com>
2023-09-09 11:51:14 +0300

    UTC time with 00:00

--- User-Information-api/app.py
+++ User-Information-api/app.py
@@ -1,23 +1,21 @@
 from fastapi import FastAPI, Query
 
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timezone
 
 
 app = FastAPI()
 
-now = datetime.now()
 day_of_week = datetime.now().strftime("%A")
 
-utc_time = now.replace(tzinfo=timezone.utc)
-
-iso_utc_time = utc_time.isoformat()[:-5] + 'Z'
+now = datetime.now(timezone.utc)
+formatted_utc_time = now.strftime('%Y-%m-%dT%H:%M:%S%z').replace('+0000', '+00:00')
 
 @app.get("/api")
 def respond(slack_name: str = Query(...), track: str = Query(...)):
     return {
         "slack_name": f"{slack_name}",
         "current_day": f"{day_of_week}",
-        "utc_time": f"{iso_utc_time}",
+        "utc_time": f"{formatted_utc_time}",
         "track": f"{track}",
         "github_file_url": "https://github.com/atonya-bravin/HNGx-backend-track/blob/main/User-Information-api/app.py",
         "github_repo_url": "https://github.com/atonya-bravin/HNGx-backend-track/tree/main",
