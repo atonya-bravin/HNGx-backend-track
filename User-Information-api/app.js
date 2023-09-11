9fe7923039b7130b31aa6ddec669232de0639097
atonya-bravin <bravinatonya.brani@gmail.com>
2023-09-11 19:00:59 +0300

    node application api

---
+++ User-Information-api/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const moment = require("moment");
+
+const port  = process.env.PORT || 3000;
+
+const app = express();
+
+app.listen(port, ()=>{
+    console.log(`App is listening to port ${port}`);
+});
+
+let day = moment().day();
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+day = daysOfWeek[day];
+
+utc_time = moment.utc().format();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my HNGx the API is working!');
+});
+
+app.get('/api', (req, res) => {
+    const githubFileUrl = "https://github.com/atonya-bravin/HNGx-backend-track/blob/main/User-Information-api/app.js";
+    const githubRepoUrl = "https://github.com/atonya-bravin/HNGx-backend-track/tree/main";
+
+    const {slackName, track} = req.query;
+    const response = {
+        slack_name: slackName,
+        current_day: day,
+        utc_time: utc_time,
+        track: track,
+        github_file_url: githubFileUrl,
+        github_repo_url: githubRepoUrl,
+        status_code: 200,
+      };
+      res.json(response);
+});
+
+
+
