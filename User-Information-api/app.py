388dfdca8dabaf0206bf4d365805a4c8bdbca4ea
atonya-bravin <bravinatonya.brani@gmail.com>
2023-09-11 18:27:42 +0300

    reverted to using datetime and timezone

--- User-Information-api/app.py
+++ User-Information-api/app.py
@@ -1,22 +1,23 @@
 import arrow
 from fastapi import FastAPI, Query
 
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 app = FastAPI()
 
 day_of_week = datetime.now().strftime("%A")
 
-utc_time = arrow.utcnow()
-formated_utc_time = utc_time.to("UTC").format("YYYY-MM-DDTHH:mm:ss") + "Z"
+utc_time = datetime.now(timezone.utc)
+formatted_utc_time = utc_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+
 
 @app.get("/api")
 def respond(slack_name: str = Query(...), track: str = Query(...)):
     return {
         "slack_name": f"{slack_name}",
         "current_day": f"{day_of_week}",
-        "utc_time": formated_utc_time,
+        "utc_time": formatted_utc_time,
         "track": f"{track}",
         "github_file_url": "https://github.com/atonya-bravin/HNGx-backend-track/blob/main/User-Information-api/app.py",
         "github_repo_url": "https://github.com/atonya-bravin/HNGx-backend-track/tree/main",
