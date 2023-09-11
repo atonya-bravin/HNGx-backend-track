d3ef5c16ff341dac0fe8adcaf5b64ddbcda3c49f
atonya-bravin <bravinatonya.brani@gmail.com>
2023-09-11 19:58:02 +0300

    rewrite of the whole file

--- User-Information-api/app.js
+++ User-Information-api/app.js
@@ -1,42 +1,42 @@
 const express = require("express");
-
 const moment = require("moment");
+
+const app = express();
 
 const port  = process.env.PORT || 3000;
 
-const app = express();
+app.get('/api', (req, res) => {
+  const slackName = req.query.slack_name;
+  const track = req.query.track;
+
+  // Check if slackName is missing
+  if (!slackName) {
+    res.status(400).json({
+      error: 'Kindly check your slack_name and try again',
+    });
+    return;
+  }
+
+  const day = moment().utc();
+  const utc_time = day.format('YYYY-MM-DDTHH:mm:ss[Z]');
+
+  const current_day = moment().format('dddd');
+
+  // JSON response construction
+  const response = {
+    slack_name: slackName,
+    current_day: current_day,
+    utc_time: utc_time,
+    track: track,
+    github_file_url: 'https://github.com/atonya-bravin/HNGx-backend-track/blob/main/User-Information-api/app.js',
+    github_repo_url: 'https://github.com/atonya-bravin/HNGx-backend-track/tree/main',
+    status_code: 200,
+  };
+
+  // Send the JSON response
+  res.json(response);
+});
 
 app.listen(port, ()=>{
     console.log(`App is listening to port ${port}`);
 });
-
-let day = moment().day();
-const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-day = daysOfWeek[day];
-
-utc_time = moment().utc().format('YYYY-MM-DDTHH:mm:ss[Z]');
-
-app.get('/', (req, res) => {
-    res.send('Welcome to my HNGx the API is working!');
-});
-
-app.get('/api', (req, res) => {
-    const githubFileUrl = "https://github.com/atonya-bravin/HNGx-backend-track/blob/main/User-Information-api/app.js";
-    const githubRepoUrl = "https://github.com/atonya-bravin/HNGx-backend-track/tree/main";
-
-    const {slack_name, track} = req.query;
-    const response = {
-        slack_name: slack_name,
-        current_day: day,
-        utc_time: utc_time,
-        track: track,
-        github_file_url: githubFileUrl,
-        github_repo_url: githubRepoUrl,
-        status_code: 200,
-      };
-      res.json(response);
-});
-
-
-
